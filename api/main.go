a10cd049cb84f7f7725a2001dd7226854130ffd0
Gabriel Poeta <95362336+gabrielpoetaa@users.noreply.github.com>
2025-01-03 16:06:29 -0500

    feat: add cors compatibility

--- api/main.go
+++ api/main.go
@@ -9,10 +9,16 @@ import (
 )
 
 func main() {
+	// Carrega as variáveis de ambiente
 	config.LoadEnv()
 
+	// Gera o roteador
 	r := router.GerarRouter()
-	fmt.Printf("Listening at port %d", config.Port)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
+	// Aplica o middleware CORS no roteador (agora r é *mux.Router, e CORS será aplicado)
+	handlerWithCORS := config.ConfigCORS(r)
+
+	// Inicia o servidor na porta configurada
+	fmt.Printf("Servidor rodando na porta %d\n", config.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), handlerWithCORS))
 }
--- api/src/config/config.go
+++ api/src/config/config.go
@@ -2,8 +2,12 @@ package config
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
 	"github.com/joho/godotenv"
 )
@@ -26,10 +30,23 @@ func LoadEnv() {
 	}
 
 	DbConnectionString = os.Getenv("STRING_CONEXAO")
-	
+
+
 	// fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
 	// 	os.Getenv("DB_USER"),
 	// 	os.Getenv("DB_PASSWORD"),
 	// 	os.Getenv("DB_NOME"),
 	// )
+}
+
+func ConfigCORS(r *mux.Router) http.Handler {
+	// Configurações de CORS
+	corsOptions := cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"}, // Origem permitida (frontend)
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"}, // Métodos permitidos
+		AllowedHeaders: []string{"Content-Type", "Authorization"}, // Cabeçalhos permitidos
+	}
+
+	// Aplica o middleware CORS no roteador e retorna
+	return cors.New(corsOptions).Handler(r) // Retorna o http.Handler com CORS aplicado
 }
