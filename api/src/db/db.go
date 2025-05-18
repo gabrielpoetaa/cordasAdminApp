121ed107d50e9228bdb5ac68f13bab88612f44aa
Gabriel Poeta <95362336+gabrielpoetaa@users.noreply.github.com>
2025-05-18 07:04:59 -0400

    feat: update db.go with postgres libraries

--- api/src/db/db.go
+++ api/src/db/db.go
@@ -3,12 +3,19 @@ package db
 import (
 	"api/src/config"
 	"database/sql"
+	"strings"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
-func Conn() (*sql.DB, error)  {
-	db, err := sql.Open("mysql", config.DbConnectionString)
+func Conn() (*sql.DB, error) {
+	// Ensure the connection string uses the correct prefix for pq driver
+	connectionString := config.DbConnectionString
+	if strings.HasPrefix(connectionString, "postgresql://") {
+		connectionString = "postgres://" + connectionString[13:]
+	}
+
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		return nil, err
 	}
@@ -18,5 +25,5 @@ func Conn() (*sql.DB, error)  {
 		return nil, err
 	}
 
-	return db,nil
+	return db, nil
 }
